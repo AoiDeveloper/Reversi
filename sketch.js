d9c941a9194c44fe45778aa24408791b399c6f64
BuildTools <unconfigured@null.spigotmc.org>
2021-02-07 19:54:03 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,33 @@
+let Cells = [
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+]
+
 function setup(){
     createCanvas(800, 500);
 }
   
 function draw(){
 
+}
+
+let drawBoard = () => {
+    for(let i = 0; i < 8; i++) {
+        for(let j = 0; j < 8; j++) {
+            drawCell(i, j);
+        }
+    }
+}
+
+// x:i, y:jのCellを描画します。返り値はなし
+let drawCell = (i, j) => {
+    let info = Cells[j, i];
+    fill(0, 255, 0);
+    rect(i * 80, j * 80, 80, 80);
 }
